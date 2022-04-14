6b21006ef450b7935b7155fa16f2d085ef12042d
Reels71 <victoruzodinma71@gmail.com>
2022-04-14 16:16:52 +0100

    second commit

--- Type casting.py
+++ Type casting.py
@@ -8,7 +8,7 @@ celsius_value = input("temperature value in celsius = ")
 
 print ((int(celsius_value) * 1.8) + 32)
 
-print (int(celsius_value) * (9/5) + 32)
+print (float(celsius_value) * (9/5) + 32)
 
 fahrenheit_value = input("final answer in fahrenheit is = ")
 
